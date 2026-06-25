3777f34731a0fd6d66c1c522a0325f146f9f9877
sameerpa <sameerpa2003@.com>
2026-06-26 00:11:55 +0530

    Fix uploads folder for Render

--- Server/middleware/uploadMiddleware.js
+++ Server/middleware/uploadMiddleware.js
@@ -1,42 +1,45 @@
 const multer = require("multer");
+const fs = require("fs");
+
+if (!fs.existsSync("uploads")) {
+  fs.mkdirSync("uploads");
+}
 
 const storage = multer.diskStorage({
-destination: (req, file, cb) => {
-cb(null, "uploads/");
-},
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
 
-filename: (req, file, cb) => {
-cb(
-null,
-Date.now() + "-" + file.originalname
-);
-},
+  filename: (req, file, cb) => {
+    cb(
+      null,
+      Date.now() + "-" + file.originalname
+    );
+  },
 });
 
 const fileFilter = (req, file, cb) => {
-const allowedTypes = [
-"application/pdf",
-"image/jpeg",
-"image/jpg",
-"image/png",
-];
+  const allowedTypes = [
+    "application/pdf",
+    "image/jpeg",
+    "image/jpg",
+    "image/png",
+  ];
 
-if (
-allowedTypes.includes(file.mimetype)
-) {
-cb(null, true);
-} else {
-cb(
-new Error(
-"Only PDF, JPG, JPEG and PNG files are allowed"
-)
-);
-}
+  if (allowedTypes.includes(file.mimetype)) {
+    cb(null, true);
+  } else {
+    cb(
+      new Error(
+        "Only PDF, JPG, JPEG and PNG files are allowed"
+      )
+    );
+  }
 };
 
 const upload = multer({
-storage,
-fileFilter,
+  storage,
+  fileFilter,
 });
 
 module.exports = upload;
